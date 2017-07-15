95ba72fc4ac91f9afae748d49ef4f848eadca2b4
Jonathan56 <jonathan.coignard@gmail.com>
2017-07-14 18:29:39 -0700

    added Mars, in progress for logging feature

--- example/1_demo/script.py
+++ example/1_demo/script.py
@@ -1,13 +1,14 @@
 from __future__ import division
 import pandas
+import datetime as dt
 import marslogistic.simulation as m
 
 # Read timeline
-timeline = pandas.read_excel('timeline.xlsx', sheetname=None)
+timeline = pandas.read_excel('timeline.xlsx')
 
 # Create simulation
-simulation = m.Simulation(
-    'db', timeline['initialization'], 'timeline')
+simulation = m.Simulation('db', timeline)
 
 # Start simulation
+simulation.end = dt.datetime(2028, 1, 1, 0, 0, 0)
 simulation.run()
